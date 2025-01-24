168f578c957acdd3aa6e846727a68642ee4490d8
AartiShintre <aartishintre100@gmail.com>
2025-01-24 18:15:34 +0530

    Add files via upload

---
+++ mathematicaloperation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"enter frist number";
+	cin>>n1;
+		cout<<"enter second number";
+	cin>>n2;
+	int sum,sub,mul,div;
+	sum=n1+n2;
+	cout<<"summation is "<<"\t"<<sum<<endl;
+	sub=n1-n2;
+	cout<<"substration is "<<"\t"<<sub<<endl;
+	mul=n1*n2;
+	cout<<"multipication is "<<"\t"<<mul<<endl;
+	div=n1/n2;
+	cout<<"division is "<<"\t"<<div<<endl;
+}
+
