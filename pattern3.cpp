d91f7c31496028d67c52039291f8c5aa3aea616a
AartiShintre <aartishintre100@gmail.com>
2025-01-26 21:50:07 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	//char ch='A';
+
+	for(i=0;i<3;i++)
+	{
+	//	char ch='A';
+		for(j=0;j<3;j++)
+		{	
+			char ch='A';
+			cout<<ch;
+			ch++;
+			
+		}
+	cout<<endl;
+	}
+}
