27455d5c071b8ce709181a1f5c586ad427e7bf15
AartiShintre <aartishintre100@gmail.com>
2025-02-07 13:11:44 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+int i;
+	cout<<"enter a number";
+		cin>>n;
+		cout<<n;
+		i=n;
+	while(i<n*10)
+	{
+		i=i+n;
+		cout<<"\n"<<i;
+	}
+
+}
