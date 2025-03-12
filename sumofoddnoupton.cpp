5dc8bce89d57fbda101e9014853369dda412898b
AartiShintre <aartishintre100@gmail.com>
2025-03-12 09:46:06 -0700

    Add files via upload

---
+++ sumofoddnoupton.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter number";
+	cin>>n;
+	int i=1;
+	int temp=0;
+	while(i<n)
+	{
+	
+		temp=temp+i;
+			i=i+2;
+	
+	}
+	
+		cout<<temp;
+}
