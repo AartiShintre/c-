5dd814d4b1cd9902fd1c3232578bc5e7671ff60c
AartiShintre <aartishintre100@gmail.com>
2025-03-22 12:39:53 +0530

    Add files via upload

---
+++ stringbasic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*string str="arti shintre";
+	cout<<str<<endl;
+	string str1="hi";
+	string str2="bye";
+	string str3=str1+str2;
+	cout<<str3;*/
+/*	string str;
+	cout<<"enter input";
+	cin>>str;
+	cout<<"output:"<<str;*/
+	string str;
+	getline(cin,str);
+	cout<<"output:"<<str;
+	
+}
