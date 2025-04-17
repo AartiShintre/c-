34767e6380b4b16411065ba18dbdf3c8e3aead8a
AartiShintre <aartishintre100@gmail.com>
2025-04-17 16:21:53 +0530

    Add files via upload

---
+++ all_operation_vector.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	/*vector<int> vec ={1,2,3,4,5};
+	for(int i=0;i<vec.size();i++){
+	  cout<< vec[i];
+	}*/
+
+//cout<<vec.size();
+
+ /* vector<int> vec2(5, 9);  
+
+    for (int i = 0; i < vec2.size(); i++) {
+        cout << vec2[i] << " ";  
+    }*/
+
+/*	vector<int>vec3={4,7,9,1,3};
+	sort(vec3.begin(),vec3.end());
+	for(int i=0;i<vec3.size();i++){
+	    cout<<vec3[i];
+	}*/
+ /* vector<int> vec3 = {4, 7, 9, 1, 3};
+
+    reverse(vec3.begin(), vec3.end());
+
+    for (int i = 0; i < vec3.size(); i++) {
+        cout << vec3[i] << " ";
+    }*/
+	
+/*	vector<int>vec3={4,7,9,1,3};
+    vec3.push_back(10);
+    for(int i=0;i<vec3.size();i++){
+	    cout<<" "<<vec3[i];	}
+	    cout<<endl;
+    vec3.pop_back();
+	for(int i=0;i<vec3.size();i++){
+	    cout<<" "<<vec3[i];	}
+	    cout<<endl;
+	   vec3.insert(vec3.begin() + 2, 8);  // Inserts 8 at index 2
+    for (int i = 0; i < vec3.size(); i++) {
+        cout << " " << vec3[i];  }
+	    cout<<endl;
+	    vec3.insert(vec3.end()-1,9);
+	 for(int i=0;i<vec3.size();i++){
+	        cout<<" "<<vec3[i];
+	    }
+	    cout<<endl;
+	    vec3.erase(vec3.begin()+3);
+	    	 for(int i=0;i<vec3.size();i++){
+	        cout<<" "<<vec3[i];
+	    }*/
+	   /* vector<int>vec3={4,7,9,1,3};
+	    vec3.erase(vec3.end()+3);
+	    	 for(int i=0;i<vec3.size();i++){
+	        cout<<" "<<vec3[i];
+	    }*/
+	    /*vector<int>vec1={1,2,3,4,5};
+	    cout<<vec1.size()<<endl;
+	    cout<<vec1.capacity();*/
+	  
+	return 0;
+}
